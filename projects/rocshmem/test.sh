f97b8115c91cc5ae6e280dd083fad9e693acd226
amirsojoodi <amir.sojoodi@amd.com>
2026-03-30 15:35:09 -0400

    Dev: add more cases to the test

--- projects/rocshmem/test.sh
+++ projects/rocshmem/test.sh
@@ -3,31 +3,51 @@
 set -eux
 
 # export GPU_DEVICE_ORDINAL="0"
-
-mpirun -n 2 -mca pml ucx -mca osc ucx -x ROCSHMEM_MAX_NUM_CONTEXTS=1 \
- -x UCX_ROCM_IPC_SIGPOOL_MAX_ELEMS=16384 -x ROCSHMEM_HEAP_SIZE=68719476736 \
- -x ROCSHMEM_TEST_UUID=1 --timeout 300 --map-by numa \
- ./build/tests/functional_tests/rocshmem_functional_tests -a 26 -w 1 -z 1024 \
- -s 1073741824 2>&1 | tee ./tests-results/logs-heatmap-loadstore/wgput_n2_w1_z1024_1073741824B.log
+LOG_DIR=./tests-results/logs-heatmap-loadstore/
+mkdir -p $LOG_DIR
 
 mpirun -n 2 -mca pml ucx -mca osc ucx -x ROCSHMEM_MAX_NUM_CONTEXTS=1 \
  -x UCX_ROCM_IPC_SIGPOOL_MAX_ELEMS=16384 -x ROCSHMEM_HEAP_SIZE=68719476736 \
  -x ROCSHMEM_TEST_UUID=1 --timeout 300 --map-by numa \
  ./build/tests/functional_tests/rocshmem_functional_tests -a 26 -w 1 -z 64 \
- -s 1073741824 2>&1 | tee ./tests-results/logs-heatmap-loadstore/wgput_n2_w1_z64_1073741824B.log
+ -s 1073741824 2>&1 | tee $LOG_DIR/wgput_n2_w1_z64_1073741824B.log
   
 mpirun -n 2 -mca pml ucx -mca osc ucx -x ROCSHMEM_MAX_NUM_CONTEXTS=2 \
  -x UCX_ROCM_IPC_SIGPOOL_MAX_ELEMS=16384 -x ROCSHMEM_HEAP_SIZE=68719476736 \
  -x ROCSHMEM_TEST_UUID=1 --timeout 300 --map-by numa \
  ./build/tests/functional_tests/rocshmem_functional_tests -a 26 -w 2 -z 64 \
- -s 1073741824 2>&1 | tee ./tests-results/logs-heatmap-loadstore/wgput_n2_w2_z64_1073741824B.log
+ -s 1073741824 2>&1 | tee $LOG_DIR/wgput_n2_w2_z64_1073741824B.log
+
+mpirun -n 2 -mca pml ucx -mca osc ucx -x ROCSHMEM_MAX_NUM_CONTEXTS=1 \
+ -x UCX_ROCM_IPC_SIGPOOL_MAX_ELEMS=16384 -x ROCSHMEM_HEAP_SIZE=68719476736 \
+ -x ROCSHMEM_TEST_UUID=1 --timeout 300 --map-by numa \
+ ./build/tests/functional_tests/rocshmem_functional_tests -a 26 -w 1 -z 1024 \
+ -s 1073741824 2>&1 | tee $LOG_DIR/wgput_n2_w1_z1024_1073741824B.log
+ 
+mpirun -n 2 -mca pml ucx -mca osc ucx -x ROCSHMEM_MAX_NUM_CONTEXTS=2 \
+ -x UCX_ROCM_IPC_SIGPOOL_MAX_ELEMS=16384 -x ROCSHMEM_HEAP_SIZE=68719476736 \
+ -x ROCSHMEM_TEST_UUID=1 --timeout 300 --map-by numa \
+ ./build/tests/functional_tests/rocshmem_functional_tests -a 26 -w 2 -z 1024 \
+ -s 1073741824 2>&1 | tee $LOG_DIR/wgput_n2_w2_z1024_1073741824B.log
+
+mpirun -n 2 -mca pml ucx -mca osc ucx -x ROCSHMEM_MAX_NUM_CONTEXTS=8 \
+ -x UCX_ROCM_IPC_SIGPOOL_MAX_ELEMS=16384 -x ROCSHMEM_HEAP_SIZE=68719476736 \
+ -x ROCSHMEM_TEST_UUID=1 --timeout 300 --map-by numa \
+ ./build/tests/functional_tests/rocshmem_functional_tests -a 27 -w 8 -z 512 \
+ -s 1073741824 2>&1 | tee ./tests-results/logs-heatmap-loadstore-fix/wgput_n2_w8_z512_1073741824B.log
 
 mpirun -n 2 -mca pml ucx -mca osc ucx -x ROCSHMEM_MAX_NUM_CONTEXTS=16 \
  -x UCX_ROCM_IPC_SIGPOOL_MAX_ELEMS=16384 -x ROCSHMEM_HEAP_SIZE=68719476736 \
  -x ROCSHMEM_TEST_UUID=1 --timeout 300 --map-by numa \
  ./build/tests/functional_tests/rocshmem_functional_tests -a 26 -w 16 -z 1024 \
- -s 1073741824 2>&1 | tee ./tests-results/logs-heatmap-loadstore/wgput_n2_w16_z1024_1073741824B.log
+ -s 1073741824 2>&1 | tee $LOG_DIR/wgput_n2_w16_z1024_1073741824B.log
 
+
+# mpirun -n 2 -mca pml ucx -mca osc ucx -x ROCSHMEM_MAX_NUM_CONTEXTS=16 \
+#  -x UCX_ROCM_IPC_SIGPOOL_MAX_ELEMS=16384 -x ROCSHMEM_HEAP_SIZE=68719476736 \
+#  -x ROCSHMEM_TEST_UUID=1 --timeout 300 --map-by numa \
+#  ./build/tests/functional_tests/rocshmem_functional_tests -a 26 -w 16 -z 1024 \
+#  -s 1073741824 2>&1 | tee ./tests-results/logs-heatmap-loadstore-fix/wgput_n2_w16_z1024_1073741824B.log
 
 # mpirun -n 2 -mca pml ucx -mca osc ucx -x ROCSHMEM_MAX_NUM_CONTEXTS=1 \
 #  -x UCX_ROCM_IPC_SIGPOOL_MAX_ELEMS=16384 -x ROCSHMEM_HEAP_SIZE=68719476736 \
