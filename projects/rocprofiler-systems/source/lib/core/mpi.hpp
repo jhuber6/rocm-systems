f7ca59b06649e7679994de2fe547d63566e820b7
Mikhail <lloginov@amd.com>
2026-04-03 05:04:41 -0400

    add exception message

--- projects/rocprofiler-systems/source/lib/core/mpi.hpp
+++ projects/rocprofiler-systems/source/lib/core/mpi.hpp
@@ -762,8 +762,8 @@ get_rank_from_env()
             return rank;
         } catch(const std::exception& e)
         {
-            LOG_WARNING("MPI output filtering: failed to get MPI rank from {}='{}'",
-                        env_var, rank_str);
+            LOG_WARNING("MPI output filtering: failed to get MPI rank from {}='{}': {}",
+                        env_var, rank_str, e.what());
         }
     }
 
